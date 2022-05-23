8d4701561454a17f9debc59772bbff81420ac5a5
bosheng <boshengwu1994@gmail.com>
2022-05-22 22:55:27 -0400

    add Practice 1 (Image Store Design) - Learn Interface and abstract method

---
+++ Basic/src/Interface/LearnInterface.java
@@ -0,0 +1,12 @@
+package Interface;
+
+/**
+ * Cannot define in interface
+ * 1. field
+ * 2. static method
+ * 3. private method
+ * 4. implemented method
+ * */
+public interface LearnInterface {
+
+}
---
+++ Basic/src/Interface/Practice-Image-Store.md
@@ -0,0 +1,4 @@
+### Practice 1. Design an image store
+#### Requirement
+- ImageStore which will have multiple cloud option(GCP, AWS)
+- ImageStore might have multiple image format
